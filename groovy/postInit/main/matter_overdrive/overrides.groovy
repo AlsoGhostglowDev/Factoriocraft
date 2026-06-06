c7ce998531b7b9d5972d9c311354dd3d687d836a
GhostDev <glowinghost@gmail.com>
2026-06-06 15:48:16 +0800

    tweaks, mod removal / additions

--- groovy/postInit/main/matter_overdrive/overrides.groovy
+++ groovy/postInit/main/matter_overdrive/overrides.groovy
@@ -5,3 +5,6 @@ OverdriveBioticStats.zeroCalories.getRequiredItems().tap {
     clear()
     add(item('matteroverdrive:emergency_ration') * 8)
 }
+
+// disable light level for industrial glass 10 -> 0
+block('matteroverdrive:industrial_glass').setLightLevel(0f)
